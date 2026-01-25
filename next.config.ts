f2d45d01a8c08244370ddfc3d3b591abf4ab1a6d
Aiyern30 <ianbian2@gmail.com>
2026-01-25 15:47:24 +0800

    feat: Add lh3.googleusercontent.com to Next.js image remote patterns.

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,11 @@ const nextConfig: NextConfig = {
         hostname: "maps.googleapis.com",
         pathname: "/maps/api/**",
       },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
+      },
     ],
   },
 };
